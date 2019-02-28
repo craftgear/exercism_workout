cd9ddd6c66be0cd7ce8329d143192b8fd3122d9f
Shunsuke Watanabe <craftgear@gmail.com>
2019-02-28 16:49:39 +0900

    remove unncessary documents

--- haskell/M.Hiroi_Haskell/www.geocities.jp/m_hiroi/light/canvas01.js
+++
@@ -1,8 +0,0 @@
-function draw() {
-  let c = document.getElementById("can");
-  if (!c.getContext) return false;
-  let ctx = c.getContext("2d");
-  ctx.fillRect(10, 10, 180, 180);     // 塗りつぶし
-}
-
-window.onload = draw();
