1daf8a4e168fa8c23187efc129decae1e2567cef
Shunsuke Watanabe <craftgear@gmail.com>
2019-03-17 09:10:31 +0000

    use iterate

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -8,4 +8,11 @@ collatzHelper t x | x == 1    = Just t
 
 collatz :: Integer -> Maybe Integer
 collatz x | x <= 0    = Nothing
-          | otherwise = collatzHelper 0 x
+          | otherwise = Just (helper x)
+
+helper :: Integer -> Integer
+helper initialValue =
+  fromIntegral . length $ takeWhile (> 1) $ iterate iterateFn initialValue
+
+iterateFn x | even x    = x `quot` 2
+            | otherwise = x * 3 + 1
