05e79e18c5be4e5689cf4e65e8a56635bde47570
Shunsuke Watanabe <craftgear@gmail.com>
2019-03-18 13:10:31 +0000

    use iterate #2

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,11 +1,5 @@
 module CollatzConjecture (collatz) where
 
-
-collatzHelper :: Integer -> Integer -> Maybe Integer
-collatzHelper t x | x == 1    = Just t
-                  | even x    = collatzHelper (t + 1) (x `div` 2)
-                  | otherwise = collatzHelper (t + 1) (x * 3 + 1)
-
 collatz :: Integer -> Maybe Integer
 collatz x | x <= 0    = Nothing
           | otherwise = Just (helper x)
