4af5a547b3da7a4fd1edcfe3352bc6e34f8b9f07
Shunsuke Watanabe <craftgear@gmail.com>
2019-03-22 16:31:23 +0900

    Haskell: rna-transcription #3

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -3,8 +3,6 @@ module DNA (toRNA) where
 toRNA :: String -> Either Char String
 toRNA xs = foldr translate (Right "") xs
 
-{- I don't come up with a solution without recursion. -}
-{- Is there any way to eliminate recursion?  -}
 translate :: Char -> Either Char String -> Either Char String
 translate x accum = case x of
   'C'       -> fmap (\xs -> 'G' : xs) accum
