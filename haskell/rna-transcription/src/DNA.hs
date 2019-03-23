558591874d244a1e5c7ddd92de41fb5c0e284bc9
Shunsuke Watanabe <craftgear@gmail.com>
2019-03-23 11:12:26 +0900

    Haskell: rna transcription #5

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,12 +1,26 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs = foldr translate (Right "") xs
+toRNA xs = foldr helper (Right "") xs
+ where
+  helper x accum = case transcribe x of
+    Right x   -> fmap (\xs -> x : xs) accum
+    otherwise -> Left x
 
-translate :: Char -> Either Char String -> Either Char String
-translate x accum = case x of
-  'C'       -> fmap (\xs -> 'G' : xs) accum
-  'G'       -> fmap (\xs -> 'C' : xs) accum
-  'T'       -> fmap (\xs -> 'A' : xs) accum
-  'A'       -> fmap (\xs -> 'U' : xs) accum
+{- translate :: Char -> Either Char String -> Either Char String -}
+{- translate x accum = case x of -}
+  {- 'C'       -> fmap (\xs -> 'G' : xs) accum -}
+  {- 'G'       -> fmap (\xs -> 'C' : xs) accum -}
+  {- 'T'       -> fmap (\xs -> 'A' : xs) accum -}
+  {- 'A'       -> fmap (\xs -> 'U' : xs) accum -}
+  {- otherwise -> Left x -}
+
+transcribe :: Char -> Either Char Char
+transcribe x = case x of
+  'C'       -> Right 'G'
+  'G'       -> Right 'C'
+  'T'       -> Right 'A'
+  'A'       -> Right 'U'
   otherwise -> Left x
+
+
